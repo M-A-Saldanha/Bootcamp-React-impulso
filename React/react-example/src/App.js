51d8b4187450b5088edad9566bb66d2502f359e9
M-A-Saldanha <marco.antonio.saldanha@hotmail.com>
2021-11-04 23:15:31 -0300

    Aulas React

---
+++ React/react-example/src/App.js
@@ -0,0 +1,24 @@
+import Item from './components/item'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <>
+      <h1>Exemplo de aplicação com React</h1>
+      <ul>
+        <Item>
+          Item 1
+        </ Item>
+        <Item>
+          Item 2
+        </ Item>
+        <Item>
+          Item 3
+        </ Item>
+      </ul>
+      <Card />
+    </>
+  )
+}
+
+export default App;
